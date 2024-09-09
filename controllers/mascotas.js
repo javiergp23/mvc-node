0b7cb79eb564dbda7afc528d0a267fec8c8ce168
Javier Gonzalez <javgon2306@gmail.com>
2024-09-09 14:39:12 -0300

    complete CRUD

--- controllers/mascotas.js
+++ controllers/mascotas.js
@@ -15,28 +15,35 @@ class mascotasController{
 
     async update(req, res){
         try{
-            res.status(201).json({ status: 'update-ok'});
+            const id = req.params
+            const data = await mascotasModel.update(id, req.body);
+            res.status(200).json(data);
         }catch(error){
             res.status(500).send(error);
         }
     }
     async getAll(req, res){
         try{
-            res.status(201).json({ status: 'getAll-ok'});
+            const data = await mascotasModel.getAll();
+            res.status(201).json(data);
         }catch(error){
             res.status(500).send(error);
         }
     }
     async getOne(req, res){
         try{
-            res.status(201).json({ status: 'getOne-ok'});
+            const { id } = req.params;
+            const data = await mascotasModel.getOne(id);
+            res.status(201).json(data);
         }catch(error){
             res.status(500).send(error);
         }
     }
     async delete(req, res){
         try{
-            res.status(201).json({ status: 'delete-ok'});
+            const { id } = req.params;
+            const data = await mascotasModel.delete(id)
+            res.status(206).json(data);
         }catch(error){
             res.status(500).send(error);
         }
