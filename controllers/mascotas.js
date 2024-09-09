e9e493d8055fb3c45ae8ad7657aa5fa1d29ecf47
Javier Gonzalez <javgon2306@gmail.com>
2024-09-09 11:55:11 -0300

    create controllers

---
+++ controllers/mascotas.js
@@ -0,0 +1,46 @@
+import e from "express";
+
+class mascotasController{
+    constructor(){
+
+    }
+
+    async create(req, res){
+        try{
+            res.status(201).json({ status: 'create-ok'});
+        }catch(error){
+            res.status(500).send(error);
+        }
+    }
+
+    async update(req, res){
+        try{
+            res.status(201).json({ status: 'update-ok'});
+        }catch(error){
+            res.status(500).send(error);
+        }
+    }
+    async getAll(req, res){
+        try{
+            res.status(201).json({ status: 'getAll-ok'});
+        }catch(error){
+            res.status(500).send(error);
+        }
+    }
+    async getOne(req, res){
+        try{
+            res.status(201).json({ status: 'getOne-ok'});
+        }catch(error){
+            res.status(500).send(error);
+        }
+    }
+    async delete(req, res){
+        try{
+            res.status(201).json({ status: 'delete-ok'});
+        }catch(error){
+            res.status(500).send(error);
+        }
+    }
+}
+
+export default new mascotasController();
--- routes/mascotas.js
+++ routes/mascotas.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import mascotasController from '../controllers/mascotas.js';
 
 const route = express.Router();
 
-route.post('/');
-route.get('/:id');
-route.put('/:id');
-route.delete('/:id');
+route.post('/', mascotasController.create);
+route.get('/:id', mascotasController.getOne);
+route.get('/:id', mascotasController.getAll);
+route.put('/:id', mascotasController.update);
+route.delete('/:id', mascotasController.delete);
 
